ed38329c563641d079961bfa1736a929f38fc00f
yangjianj <252089024@qq.com>
2020-06-15 17:35:50 +0800

    up

--- run.py
+++ run.py
@@ -15,24 +15,34 @@ class Runner():
         pass
     
     def run_task(self,taskparam):
-        self._build_command(SETTING.PROCESSES,taskparam.outputdir)
+        cmd_param= {}
+        cmd_param["processes"] = SETTING.PROCESSES
+        cmd_param["lib"] = SETTING.LIB
+        cmd_param["listener"] = SETTING.LISTENER
+        cmd_param["outputdir"] = taskparam["outputdir"]
+        cmd_param["taskname"] = taskparam["taskname"]
+        cmd_param["include"] = taskparam["include"]
+        cmd_param["suitedir"] = taskparam["suitedir"]
+        cmd_param["variable"] = taskparam["variable"]
+        self._build_command(cmd_param)
+        self.run()
     
-    def _build_command(self,processes,outputdir,taskname,lib,listener,include,rootdir,variable={}):
+    def _build_command(self,cmd_param):
         result = {}
-        result["process"]= " --processes "+processes
-        result["outputdir"] = " --outputdir "+outputdir
-        result["taskname"] = " --name "+taskname
-        result["lib"] = " --pythonpath "+lib
-        result["listener"] = " --listener "+listener
-        result["include"] = " --include "+include
+        result["processes"]= " --processes "+cmd_param['processes']
+        result["outputdir"] = " --outputdir "+cmd_param['outputdir']
+        result["taskname"] = " --name "+cmd_param['taskname']
+        result["lib"] = " --pythonpath "+cmd_param['lib']
+        result["listener"] = " --listener "+cmd_param['listener']
+        result["include"] = " --include "+cmd_param['include']
         result["variable"] = ''
-        for k,v in variable:
+        for k,v in cmd_param['variable']:
             tmp= " --variable "+str(k)+':'+str(v)
             result["variable"] = result["variable"]+tmp
         self.cmd = self.exec_cmd + " --pabotlib "
         for k,v in result:
             self.cmd=self.cmd+v
-        self.cmd = self.cmd+ ' '+rootdir
+        self.cmd = self.cmd+ ' '+cmd_param['suitedir']
     
 
     def run(self):
